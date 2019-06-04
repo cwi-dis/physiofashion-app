4933af060c5d5541fb155e57b59cc344452c2684
Thomas Röggla <tom@lichtfragmente.com>
2019-06-04 20:26:39 +0200

    added property fabricOnSkin to Trial class

--- app/src/main/java/nl/cwi/dis/physiofashion/experiment/Trial.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/experiment/Trial.java
@@ -1,12 +1,18 @@
 package nl.cwi.dis.physiofashion.experiment;
 
 public class Trial {
+    private boolean fabricOnSkin;
     private String condition;
     private String intensity;
 
-    public Trial(String condition, String intensity) {
+    public Trial(boolean fabricOnSkin, String condition, String intensity) {
+        this.fabricOnSkin = fabricOnSkin;
         this.condition = condition;
         this.intensity = intensity;
+    }
+
+    public boolean isFabricOnSkin() {
+        return fabricOnSkin;
     }
 
     public String getIntensity() {
