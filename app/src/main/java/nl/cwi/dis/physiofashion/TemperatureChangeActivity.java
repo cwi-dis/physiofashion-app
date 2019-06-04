b4224ecf517f6da251f36182472b1341838d1b70
Thomas Röggla <tom@lichtfragmente.com>
2019-06-04 17:01:17 +0200

    added activity which will update the temperature of the board

---
+++ app/src/main/java/nl/cwi/dis/physiofashion/TemperatureChangeActivity.java
@@ -0,0 +1,13 @@
+package nl.cwi.dis.physiofashion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemperatureChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature_change);
+    }
+}
