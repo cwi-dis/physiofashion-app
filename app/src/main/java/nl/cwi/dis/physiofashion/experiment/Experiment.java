f16b2853c87d1e420450528189ee1cc6375364cf
Thomas Röggla <t.roggla@cwi.nl>
2019-06-05 17:38:31 +0200

    Experiment class now also stores counterbalance number

--- app/src/main/java/nl/cwi/dis/physiofashion/experiment/Experiment.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/experiment/Experiment.java
@@ -21,6 +21,7 @@ public class Experiment implements Parcelable {
     private ArrayList<Trial> trials;
     private String hostname;
     private String participantId;
+    private int counterBalance;
 
     private Experiment(Parcel in) {
         this.trials = new ArrayList<>();
@@ -28,12 +29,14 @@ public class Experiment implements Parcelable {
 
         this.hostname = in.readString();
         this.participantId = in.readString();
+        this.counterBalance = in.readInt();
     }
 
-    public Experiment(ArrayList<Trial> trials, String hostname, String participantId) {
+    public Experiment(ArrayList<Trial> trials, String hostname, String participantId, int counterBalance) {
         this.trials = trials;
         this.hostname = hostname;
         this.participantId = participantId;
+        this.counterBalance = counterBalance;
     }
 
     @Override
@@ -41,6 +44,7 @@ public class Experiment implements Parcelable {
         dest.writeTypedList(trials);
         dest.writeString(hostname);
         dest.writeString(participantId);
+        dest.writeInt(counterBalance);
     }
 
     @Override
@@ -58,5 +62,9 @@ public class Experiment implements Parcelable {
 
     public String getParticipantId() {
         return participantId;
+    }
+
+    public int getCounterBalance() {
+        return counterBalance;
     }
 }
