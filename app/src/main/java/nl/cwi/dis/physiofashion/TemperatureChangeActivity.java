d85e75fe71f59f9ebedb30ad933f911c23f87bd8
Thomas Röggla <t.roggla@cwi.nl>
2019-06-05 18:07:30 +0200

    extracting and logging parcel in TemperatureChangeActivity

--- app/src/main/java/nl/cwi/dis/physiofashion/TemperatureChangeActivity.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/TemperatureChangeActivity.java
@@ -1,13 +1,34 @@
 package nl.cwi.dis.physiofashion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import nl.cwi.dis.physiofashion.experiment.Experiment;
+import nl.cwi.dis.physiofashion.experiment.Trial;
 
 public class TemperatureChangeActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "TemperatureChangeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_change);
+
+        Intent intent = this.getIntent();
+        final Experiment experiment = intent.getParcelableExtra("experiment");
+
+        Log.d(LOG_TAG, "Host: " + experiment.getHostname());
+        Log.d(LOG_TAG, "Participant: " + experiment.getParticipantId());
+        Log.d(LOG_TAG, "Counterbalance: " + experiment.getCounterBalance());
+
+        ArrayList<Trial> trials = experiment.getTrials();
+
+        for (Trial trial : trials) {
+            Log.d(LOG_TAG, "Trial: " + trial.isFabricOn() + " " + trial.getCondition() + " " + trial.getIntensity() + " " + trial.hasAudio());
+        }
     }
 }
