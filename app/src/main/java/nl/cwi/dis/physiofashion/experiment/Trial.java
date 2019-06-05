45317dd5e443554b688539a177f56967da7a4385
Thomas Röggla <t.roggla@cwi.nl>
2019-06-05 17:19:25 +0200

    made Trial class parcelable

--- app/src/main/java/nl/cwi/dis/physiofashion/experiment/Trial.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/experiment/Trial.java
@@ -1,14 +1,47 @@
 package nl.cwi.dis.physiofashion.experiment;
 
-public class Trial {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Trial implements Parcelable {
+    public static final Parcelable.Creator<Trial> CREATOR = new Parcelable.Creator<Trial>() {
+        @Override
+        public Trial createFromParcel(Parcel in) {
+            return new Trial(in);
+        }
+
+        @Override
+        public Trial[] newArray(int size) {
+            return new Trial[size];
+        }
+    };
+
     private String audioFile;
     private String condition;
     private int intensity;
+
+    private Trial(Parcel in) {
+        this.audioFile = in.readString();
+        this.condition = in.readString();
+        this.intensity = in.readInt();
+    }
 
     public Trial(String audioFile, String condition, int intensity) {
         this.audioFile = audioFile;
         this.condition = condition;
         this.intensity = intensity;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(audioFile);
+        dest.writeString(condition);
+        dest.writeInt(intensity);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public boolean hasAudio() {
