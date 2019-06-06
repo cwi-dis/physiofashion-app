ce2cbfade2d611473ec323ae61dbddf62127700e
Thomas Röggla <t.roggla@cwi.nl>
2019-06-06 16:04:54 +0200

    added logic to either redirect to next trial or ending activity

--- app/src/main/java/nl/cwi/dis/physiofashion/RatingActivity.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/RatingActivity.java
@@ -1,7 +1,13 @@
 package nl.cwi.dis.physiofashion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import nl.cwi.dis.physiofashion.experiment.Experiment;
+import nl.cwi.dis.physiofashion.experiment.Trial;
 
 public class RatingActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+
+        Intent intent = this.getIntent();
+        Experiment experiment = intent.getParcelableExtra("experiment");
+
+        final SeekBar temperatureSeekBar = findViewById(R.id.temperature_seek_bar);
+        final SeekBar comfortSeekBar = findViewById(R.id.comfort_seek_bar);
+        final Button ratingNextButton = findViewById(R.id.rating_next_button);
+
+        ratingNextButton.setOnClickListener(v -> {
+            experiment.incrementCurrentTrial();
+            Trial nextTrial = experiment.getCurrentTrial();
+
+            Intent nextActivity;
+
+            if (nextTrial == null) {
+                nextActivity = new Intent(this, EndingActivity.class);
+            } else {
+                nextActivity = new Intent(this, TemperatureChangeActivity.class);
+            }
+
+            nextActivity.putExtra("experiment", experiment);
+            startActivity(nextActivity);
+        });
     }
 }
