f81d4202ffe1baa5483891da03506a1be61b8ba0
Thomas Röggla <tom@lichtfragmente.com>
2019-06-07 16:46:15 +0200

    added trial number to output csv file

--- app/src/main/java/nl/cwi/dis/physiofashion/experiment/Experiment.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/experiment/Experiment.java
@@ -160,7 +160,8 @@ public class Experiment implements Parcelable {
 
             return String.format(
                     Locale.ENGLISH,
-                    "\"%s\",\"%s\",%d,%d,%.2f,%.2f,%d,%d\n",
+                    "%d,\"%s\",\"%s\",%d,%d,%.2f,%.2f,%d,%d\n",
+                    i + 1,
                     this.participantId,
                     trial.getCondition(),
                     trial.getIntensity(),
@@ -173,7 +174,7 @@ public class Experiment implements Parcelable {
         }).collect(Collectors.toCollection(ArrayList::new));
 
         String filename = this.participantId + "_" + (this.trials.get(0).isFabricOn() ? "fabricOn" : "fabricOff") + ".csv";
-        String header = "\"participant\",\"condition\",\"intensity\",\"fabricOn\",\"stimulusStarted\",\"stimulusFelt\",\"temperatureFelt\",\"comfortLevel\"\n";
+        String header = "\"trialNum\",\"participant\",\"condition\",\"intensity\",\"fabricOn\",\"stimulusStarted\",\"stimulusFelt\",\"temperatureFelt\",\"comfortLevel\"\n";
 
         Log.d(LOG_TAG, "Attempting to write responses to file: " + targetDir.getAbsolutePath() + "/" + filename);
 
