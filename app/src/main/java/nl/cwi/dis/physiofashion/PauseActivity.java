bf7e85a109364cff2a7abbd423c90cdd5aeae461
Thomas Röggla <t.roggla@cwi.nl>
2019-07-04 17:21:34 +0200

    implemented PauseActivity which halts the experiment for a certain time

---
+++ app/src/main/java/nl/cwi/dis/physiofashion/PauseActivity.java
@@ -0,0 +1,47 @@
+package nl.cwi.dis.physiofashion;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import nl.cwi.dis.physiofashion.experiment.Experiment;
+
+public class PauseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pause);
+
+        Intent intent = this.getIntent();
+        Experiment experiment = intent.getParcelableExtra("experiment");
+
+        final TextView countdownLabel = findViewById(R.id.countdown_label);
+        final Button continueButton = findViewById(R.id.continue_button);
+        continueButton.setEnabled(false);
+
+        new CountDownTimer(experiment.getBreakDuration() * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                String msg = getApplicationContext().getString(R.string.number, millisUntilFinished / 1000);
+                countdownLabel.setText(msg);
+            }
+
+            @Override
+            public void onFinish() {
+                continueButton.setEnabled(true);
+                countdownLabel.setText(R.string.zero);
+            }
+        }.start();
+
+        continueButton.setOnClickListener(v -> {
+            Intent nextActivity = new Intent(this, TemperatureChangeActivity.class);
+            nextActivity.putExtra("experiment", experiment);
+
+            startActivity(nextActivity);
+        });
+    }
+}
