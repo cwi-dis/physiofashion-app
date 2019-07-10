caf87ae8d152a3a7956db3e5f4857683ad3a52c9
Thomas Röggla <t.roggla@cwi.nl>
2019-07-10 16:25:32 +0200

    don't need to watch for scale changes anymore with defualt values

--- app/src/main/java/nl/cwi/dis/physiofashion/ManikinRatingActivity.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/ManikinRatingActivity.java
@@ -5,18 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-import java.util.Arrays;
-
 import nl.cwi.dis.physiofashion.experiment.Experiment;
 import nl.cwi.dis.physiofashion.experiment.Trial;
 import nl.cwi.dis.physiofashion.experiment.UserResponse;
 import nl.cwi.dis.physiofashion.views.SelfAssessmentManikin;
 
 public class ManikinRatingActivity extends AppCompatActivity {
-    private Button ratingNextButton;
-    private SelfAssessmentManikin arousalScale;
-    private SelfAssessmentManikin valenceScale;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +19,9 @@ public class ManikinRatingActivity extends AppCompatActivity {
         Intent intent = this.getIntent();
         Experiment experiment = intent.getParcelableExtra("experiment");
 
-        ratingNextButton = findViewById(R.id.rating_next_button);
-        ratingNextButton.setEnabled(false);
-
-        arousalScale = findViewById(R.id.manikin_arousal);
-        valenceScale = findViewById(R.id.manikin_valence);
-
-        this.watchScaleChanges();
+        final Button ratingNextButton = findViewById(R.id.rating_next_button);
+        final SelfAssessmentManikin arousalScale = findViewById(R.id.manikin_arousal);
+        final SelfAssessmentManikin valenceScale = findViewById(R.id.manikin_valence);
 
         ratingNextButton.setOnClickListener(v -> {
             UserResponse currentResponse = experiment.getCurrentUserResponse();
@@ -58,17 +48,6 @@ public class ManikinRatingActivity extends AppCompatActivity {
             nextActivity.putExtra("experiment", experiment);
             startActivity(nextActivity);
         });
-    }
-
-    private void watchScaleChanges() {
-        SelfAssessmentManikin[] scales = new SelfAssessmentManikin[] { arousalScale, valenceScale };
-
-        for (SelfAssessmentManikin scale : scales) {
-            scale.onValueSelected(value -> {
-                boolean allScalesHaveValue = Arrays.stream(scales).allMatch(e -> e.getSelectedValue() > -1);
-                ratingNextButton.setEnabled(allScalesHaveValue);
-            });
-        }
     }
 
     @Override
