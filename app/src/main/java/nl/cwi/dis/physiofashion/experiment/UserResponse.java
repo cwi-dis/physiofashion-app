8b728561bd3d2003599dee79a7af345b1712df8a
Thomas Röggla <t.roggla@cwi.nl>
2019-09-18 15:42:38 +0200

    added documentation for the UserResponse class

--- app/src/main/java/nl/cwi/dis/physiofashion/experiment/UserResponse.java
+++ app/src/main/java/nl/cwi/dis/physiofashion/experiment/UserResponse.java
@@ -3,6 +3,11 @@ package nl.cwi.dis.physiofashion.experiment;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+/**
+ * This class encapsulates a user response to a trial within an experiment object. This class
+ * provides accessor methods for all user response properties. Also note that this class
+ * extends the Parcelable interface, so its instances can be passed between activities.
+ */
 public class UserResponse implements Parcelable {
     public static final Parcelable.Creator<UserResponse> CREATOR = new Parcelable.Creator<UserResponse>() {
         @Override
@@ -23,6 +28,11 @@ public class UserResponse implements Parcelable {
     private int arousal;
     private int valence;
 
+    /**
+     * Construct a new UserResponse object from an existing Parcel object.
+     *
+     * @param in Parcel object to construct response from
+     */
     private UserResponse(Parcel in) {
         this.stimulusStarted = in.readDouble();
         this.stimulusFelt = in.readDouble();
@@ -32,9 +42,18 @@ public class UserResponse implements Parcelable {
         this.valence = in.readInt();
     }
 
+    /**
+     * Default constructor which initialises all members to their default values.
+     */
     public UserResponse() {
     }
 
+    /**
+     * Write the object to a parcel.
+     *
+     * @param dest Destination parcel
+     * @param flags Flags, ignored
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeDouble(stimulusStarted);
@@ -50,50 +69,110 @@ public class UserResponse implements Parcelable {
         return 0;
     }
 
+    /**
+     * Gets the timestamp a stimulus has started.
+     *
+     * @return Time when the stimulus started
+     **/
     public double getStimulusStarted() {
         return stimulusStarted;
     }
 
+    /**
+     * Sets the timestamp a stimulus has started.
+     *
+     * @param stimulusStarted Time when the stimulus started
+     **/
     public void setStimulusStarted(double stimulusStarted) {
         this.stimulusStarted = stimulusStarted;
     }
 
+    /**
+     * Gets the timestamp a stimulus was felt.
+     *
+     * @return Time when the stimulus was felt
+     **/
     public double getStimulusFelt() {
         return stimulusFelt;
     }
 
+    /**
+     * Sets the timestamp a stimulus was felt.
+     *
+     * @param stimulusFelt Time when the stimulus was felt
+     **/
     public void setStimulusFelt(double stimulusFelt) {
         this.stimulusFelt = stimulusFelt;
     }
 
+    /**
+     * Get the temperature the user has felt.
+     *
+     * @return The temperature felt by the user
+     */
     public int getTemperatureFelt() {
         return temperatureFelt;
     }
 
+    /**
+     * Set the temperature the user has felt.
+     *
+     * @param temperatureFelt The temperature felt by the user
+     */
     public void setTemperatureFelt(int temperatureFelt) {
         this.temperatureFelt = temperatureFelt;
     }
 
+    /**
+     * Get the comfort level the user has felt.
+     *
+     * @return The comfort level felt by the user
+     */
     public int getComfortLevel() {
         return comfortLevel;
     }
 
+    /**
+     * Set the comfort level the user has felt.
+     *
+     * @param comfortLevel The comfort level felt by the user
+     */
     public void setComfortLevel(int comfortLevel) {
         this.comfortLevel = comfortLevel;
     }
 
+    /**
+     * Get the arousal felt by the user on a Likert scale.
+     *
+     * @return The arousal felt
+     */
     public int getArousal() {
         return arousal;
     }
 
+    /**
+     * Set the arousal felt by the user on a Likert scale.
+     *
+     * @param arousal The arousal felt
+     */
     public void setArousal(int arousal) {
         this.arousal = arousal;
     }
 
+    /**
+     * Get the valence felt by the user on a Likert scale.
+     *
+     * @return The valence felt
+     */
     public int getValence() {
         return valence;
     }
 
+    /**
+     * Set the valence felt by the user on a Likert scale.
+     *
+     * @param valence The valence felt
+     */
     public void setValence(int valence) {
         this.valence = valence;
     }
